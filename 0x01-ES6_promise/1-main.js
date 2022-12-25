e15c98d12cac9c2e325fcc934159862d91a2c759
dozonalx <ukahdozzy@gmail.com>
2022-12-25 13:15:37 +0100

    task 1

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,4 +1,4 @@
-import getFullResponseFromAPI from './1-promise';
+import getFullResponseFromAPI from './1-promise.js';
 
 console.log(getFullResponseFromAPI(true));
 console.log(getFullResponseFromAPI(false));
