5b8b477249249e0b02e3394ce111d91abe4092f0
dozonalx <ukahdozzy@gmail.com>
2022-12-25 13:31:12 +0100

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,4 @@
-export default function handleResponseFromAPI (promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
