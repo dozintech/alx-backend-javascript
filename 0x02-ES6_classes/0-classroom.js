403b37bacd490a582ca56f940d6317335b1d7504
dozonalx <ukahdozzy@gmail.com>
2023-01-02 15:26:04 +0100

    task 0 classroom sizeMax

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -7,7 +7,7 @@ export default class ClassRoom {
    * @param {Number} maxStudentsSize - The maximum number of
    * students in the class.
    */
-  constructor (maxStudentsSize) {
+  constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
 }
