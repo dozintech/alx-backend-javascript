6cf30ce4b1207ab95efddd00d145746c95e36176
dozonalx <ukahdozzy@gmail.com>
2023-01-04 05:18:21 +0100

    all task done

---
+++ 0x03-ES6_promise_data_manipulation/2-main.js
@@ -0,0 +1,4 @@
+import getListStudents from "./0-get_list_students.js";
+import getStudentsByLocation from "./2-get_students_by_loc.js";
+
+const students = getListStudents();
---
+++ 0x03-ES6_promise_data_manipulation/3-main.js
@@ -0,0 +1,5 @@
+import getListStudents from "./0-get_list_students.js";
+import getStudentIdsSum from "./3-get_ids_sum.js";
+
+const students = getListStudents();
+const value = getStudentIdsSum(students);
