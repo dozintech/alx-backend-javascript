904c90936f5d335d018d363b746a041d2fd8ff52
dozonalx <ukahdozzy@gmail.com>
2023-03-22 15:52:06 +0100

    set basics

---
+++ 0x00-ES6_basics/3-default-parameter.js
@@ -0,0 +1,5 @@
+function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
+
+export default getSumOfHoods;
---
+++ 0x00-ES6_basics/4-rest-parameter.js
@@ -0,0 +1,3 @@
+export default function returnHowManyArguments(...myargs) {
+  return myargs.length;
+}
